dba0e892d17c828c9aa7f145e753a8f1cb0d6de8
yuao000 <140300438+yuao000@users.noreply.github.com>
2025-04-10 17:16:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -217,7 +217,7 @@ function downloadCSV(data) {
         csv_data.push(add_data);
     });
 
-    const csvString = csv_data.map(row => row.join(',')).join('\r\n');
+    const csvString = csv_data.map(row => row.join(',')).join('\n');
 
     const blob = new Blob([new Uint8Array([0xEF, 0xBB, 0xBF]), csvString], { type: 'text/csv;charset=utf-8;' });
     const url = URL.createObjectURL(blob);
